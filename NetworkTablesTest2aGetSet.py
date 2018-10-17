a1e7d24b031019e161b3a620ecbe5a1f063ef4e6
bugeybot <bugeybot@gmail.com>
2018-10-17 13:37:16 -0400

    Initial Commit

---
+++ NetworkTablesTest2aGetSet.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# This is a NetworkTables client (eg, the DriverStation/coprocessor side).
+# You need to tell it the IP address of the NetworkTables server (the
+# robot or simulator).
+#
+# When running, this will continue incrementing the value 'dsTime', and the
+# value should be visible to other networktables clients and the robot.
+#
+
+import sys
+import time
+from networktables import NetworkTables
+
+# To see messages from networktables, you must setup logging
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+if len(sys.argv) != 2:
+    print("Error: specify an IP to connect to!")
+    exit(0)
+
+ip = sys.argv[1]
+
+NetworkTables.initialize(server=ip)
+
+sd = NetworkTables.getTable("Test")
+
+i = 0
+while True:
+    #get the current value, add 1, and write it back out
+    i = sd.getNumber('Time2', 0)
+    i += 2
+    print('putting time:', i)
+    sd.putNumber('Time2', i)
+    time.sleep(0.5)
--- formatter.js
+++ formatter.js
@@ -1,14 +1,37 @@
-function NetworkTableObject(key, value){
-    this.key = key;
-    this.value = value;
+function NetworkTablesObject(key, value){
+    this.data = document.getElementById("dataGoesHere");
+    this.currKey = key;
+    this.currValue = value;
     this.init();
 }
 
+Object.defineProperty(NetworkTablesObject.prototype, "key", {
+    get: function() {
+        return this.currKey;
+    },
+    set: function(key) {
+        this.currKey = key;
+    }
+});
 
-NetworkTableObject.prototype.init = function(){
-    console.log(this.key + " " + this.value);
+Object.defineProperty(NetworkTablesObject.prototype, "value", {
+    get: function() {
+        return this.currValue;
+    },
+    set: function(value) {
+        this.currValue = value;
+    }
+});
+
+NetworkTablesObject.prototype.init = function(){
+    this.element = document.createElement("div");
+    this.data.appendChild(this.element);
+    this.element.innerHTML = this.key + " init " + this.value;
+    console.log(this.key + " init " + this.value);
 };
 
-NetworkTableObject.prototype.refresh = function(){
-    console.log(this.key + " " + this.value);
+NetworkTablesObject.prototype.refresh = function(){
+    this.value = NetworkTables.getValue(this.key, 0);
+    this.element.innerHTML = this.key + " refresh " + this.value;
+    console.log(this.key + " refresh " + this.value);
 };
